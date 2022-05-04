ed25f86b7866bdfedac2a4af8aad431734dc4478
ken4197562 <100388206+ken4197562@users.noreply.github.com>
2022-05-04 11:24:54 +1000

    Update Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -24,6 +24,7 @@ namespace PerfectPolicyQuizFE2022
                 opts.Cookie.HttpOnly = true;
                 opts.Cookie.IsEssential = true;
             });
+                 services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
 
           
         }
